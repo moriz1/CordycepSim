b2445a1943816785b90cb8138d856ed82d2b6270
moriz1 <tehmoriz@gmail.com>
2015-11-04 11:03:39 -0500

    Start main menu implementation
    Canvas and start button implemented

---
+++ Assets/Scripts/Core/MainMenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void StartGameClicked() {
+		if (GameStateManager.Instance != null) {
+			GameStateManager.Instance.SetGameState(GameState.Loading);
+		}
+	}
+}
