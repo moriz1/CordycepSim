505d73720acbb1240e971fec4fd445c69e133bcc
moriz1 <tehmoriz@gmail.com>
2015-11-04 20:02:14 -0500

    Added initial loading screen
    Currently the loading screen doesn’t do anything, except wait 3 seconds
    and load the game scene. In the future, this is where the game will
    load from XML, resume from save state, etc.

---
+++ Assets/Scripts/Core/LoadingScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour {
+
+	void Start() {
+		StartCoroutine ("FinishLoadingStartGame");
+	}
+
+	IEnumerator FinishLoadingStartGame() {
+		yield return new WaitForSeconds(3.0f);
+		Application.LoadLevel ("Game");
+	}
+}
